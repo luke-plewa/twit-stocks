2c5c98b0a570f623f9bf2f0c4fbe1209c5f1e94f
luke-plewa <luke.plewa@gmail.com>
2014-05-13 10:20:20 -0700

    getting tweets

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
-
+gem 'twitter'
 # Specify your gem's dependencies in twit-stocks.gemspec
 gemspec
---
+++ models/twitter_engine.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+
+class TwitterEngine
+  def self.print_search
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "QpxDmEy2sfNN4MNzKXihk0GwQ"
+      config.consumer_secret     = "vlMiEiwYA3y1iw00jwoAHoo7q33v7UX1vaPn73Dq6zLPX02oGI"
+      config.access_token        = "354589333-0EFXMVUvR5MrukVUtGHVCSunHrrzSmYVy1BE2Afq"
+      config.access_token_secret = "q4KBVMmiRQkgR2jE9cZHeQjZeXtU7FDABimiexQoEr2mo"
+    end
+
+    client.search("Gameofthrones", :result_type => "recent").take(3).each do |tweet|
+      puts tweet.text
+    end
+  end
+end
