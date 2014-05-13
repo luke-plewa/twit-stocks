5d841d4ef452d58d183afcb8672f615bd64c28b8
luke-plewa <luke.plewa@gmail.com>
2014-05-13 10:38:16 -0700

    clean rspec

--- models/twitter_engine.rb
+++ models/twitter_engine.rb
@@ -1,16 +1,24 @@
 require 'twitter'
 
 class TwitterEngine
-  def self.print_search search_string
-    client = Twitter::REST::Client.new do |config|
+
+  attr_accessor :client
+
+  def setup
+    self.client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "QpxDmEy2sfNN4MNzKXihk0GwQ"
       config.consumer_secret     = "vlMiEiwYA3y1iw00jwoAHoo7q33v7UX1vaPn73Dq6zLPX02oGI"
       config.access_token        = "354589333-0EFXMVUvR5MrukVUtGHVCSunHrrzSmYVy1BE2Afq"
       config.access_token_secret = "q4KBVMmiRQkgR2jE9cZHeQjZeXtU7FDABimiexQoEr2mo"
     end
+  end
+
+  def print_search search_string
 
     client.search(search_string, :result_type => "recent").take(3).each do |tweet|
       puts tweet.text
     end
+
+    true
   end
 end
