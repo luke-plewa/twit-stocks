7d63618af4091cbebc4a7e4444886aa69f69eff0
luke-plewa <luke.plewa@gmail.com>
2014-05-26 16:23:12 -0700

    flesh out nodes

--- lib/twit-stocks/node.rb
+++ lib/twit-stocks/node.rb
@@ -1,5 +1,20 @@
 class Node
 
-  attr_accessor :value
+  attr_accessor :weights, :inputs
+
+  def initialize weights, inputs
+    self.weights = weights
+    self.weights << 1 # bias unit weight
+    self.inputs = inputs
+    self.inputs << 1 # bias unit
+  end
+
+  def value
+    value = 0
+    inputs.each_with_index do |input, index|
+      value += weights[index] * input
+    end
+    value
+  end
 
 end
